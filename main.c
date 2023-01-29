b604b3a5f20f7efed24f07b60594c4da7b5da4dd
Marcos Aquino <marcoskim60@gmail.com>
2023-01-28 22:20:59 -0300

    Criação da Fila de comandas e suas operações

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
-#define MAX 5
+#define MAX 10
 
 typedef struct item {
     int id;
@@ -19,7 +19,14 @@ typedef struct cardapio {
 typedef struct comanda {
     int qtd;
     Item comanda[MAX];
+    float total;
 }Comanda;
+
+typedef struct fila {
+    int inicio, final,qtd;
+    Comanda comandas[MAX];
+
+}Fila;
 
 
 Cardapio* criaCardapio() {
@@ -55,7 +62,7 @@ void carregaCardapio(Cardapio *cd) {
         printf("Erro na abertura");
     }
 
-    for(int i = 0; i < 5; i++) {
+    for(int i = 0; i < MAX; i++) {
         fscanf( f, "Id = %d\n", &cd->cardapio[i].id );
         fscanf( f, "Nome = %[^\n]\n", cd->cardapio[i].nome );
         fscanf( f, "Preco = %f\n", &cd->cardapio[i].preco );
@@ -65,6 +72,52 @@ void carregaCardapio(Cardapio *cd) {
     }
     fclose(f);
 }
+
+Fila* criaFila() {
+    Fila* fi = (Fila*) malloc (sizeof(Fila));
+    if(fi != NULL) {
+        fi->inicio = 0;
+        fi->final = 0;
+        fi->qtd = 0;
+    }
+    return fi;
+}
+
+void liberaFila(Fila* fi) {
+    free(fi);
+}
+
+
+int filaCheia(Fila* fi) {
+    if(fi == NULL) return 1;
+    if(fi->qtd == MAX)
+        return 1;
+    else
+        return 0;
+}
+
+int filaVazia(Fila* fi) {
+    if(fi == NULL) return 1;
+    if(fi->qtd == 0)
+        return 0;
+    else 
+        return 0;
+}
+
+void adicionaNaFila(Fila* fi, Comanda *cmd) {
+    if(fi == NULL || cmd == NULL) return;
+    if(filaCheia(fi)) {
+        printf("Fila cheia nao foi possivel adicionar a comanda\n");
+        return;
+    }
+    fi->comandas[fi->final] = *cmd;
+    fi->final++;
+    fi->qtd++;
+    if(fi->final == MAX) {
+        fi->final = 0;
+    }
+}
+
 int buscaID (Cardapio* cd, int search, Item** it) {
     if(cd == NULL) {
         printf("nulo\n");
@@ -86,11 +139,10 @@ int buscaID (Cardapio* cd, int search, Item** it) {
     return 1;
 }
 void geradorDeComandas(Comanda *cmd, Cardapio *cd) {
-    srand(time(NULL));
     for(int i = 0; i < MAX; i++) {
         for(int j = 0 ; j < MAX; j++) {
 
-            int id = rand() % 5 + 1;
+            int id = rand() % 10 + 1;
             Item* it;
             buscaID(cd, id, &it);
             cmd->comanda[i] = *it;
@@ -101,22 +153,50 @@ void geradorDeComandas(Comanda *cmd, Cardapio *cd) {
 
 
 
+float calculaTotal(Comanda *cmd) {
+	float total = 0;
+	for (int i = 0; i < MAX; i++) {
+		total += cmd->comanda[i].preco;
+	}
+	return total;
+}
+
 int main() {
     
+    Fila *fi;
+    fi = criaFila();
+
     Cardapio* cd;
     cd = criaCardapio();
-
     carregaCardapio(cd);
 
-    printf("Valores copiados para a struct cardapio:\n");
-    for(int i = 0; i < 5; i++) {
-        printf( "Id = %d\n", cd->cardapio[i].id );
-        printf( "Nome = %s\n", cd->cardapio[i].nome );
-        printf( "Preco = R$%.2f\n", cd->cardapio[i].preco );
-        
-    }
     
+
+    
+    
+    Comanda* cmd;
+    cmd = criaComanda();
+
+    srand(time(NULL));
+    for (int i = 0; i < MAX; i++) {
+        
+        geradorDeComandas(cmd, cd);
+        adicionaNaFila(fi, cmd);
+    }
+
+    
+    for (int i = 0; i < fi->qtd; i++) {
+        printf("===============Comanda %d=============\n", i + 1);
+        for (int j = 0; j < MAX; j++) {
+            printf( "Id = %d\n", fi->comandas[i].comanda[j].id );
+            printf( "Nome = %s\n", fi->comandas[i].comanda[j].nome );
+            printf( "Preco = R$%.2f\n", fi->comandas[i].comanda[j].preco );
+        }
+        float total = calculaTotal(&fi->comandas[i]);
+        printf("total = %.2f\n", total);
+    }
     
 
     liberaCardapio(cd);
+    liberaFila(fi);
 }
